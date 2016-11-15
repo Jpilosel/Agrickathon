597844c2c2e6bf51b13b7b6ac186cc047009364d
Jpilosel <formation@pilosel.fr>
2016-11-15 11:21:50 +0100

    bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new AgriBundle\AgriBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/AgriBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AgriBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AgriBundle:Default:index.html.twig');
+    }
+}
