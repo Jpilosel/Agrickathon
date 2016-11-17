1b8e9a2d2dc20a1ef87f2d1cbfc0575fd9803b00
Jpilosel <formation@pilosel.fr>
2016-11-17 03:39:23 +0100

    implementation summernote + finalisation page détail

--- src/AgriBundle/Controller/ArticleController.php
+++ src/AgriBundle/Controller/ArticleController.php
@@ -5,7 +5,8 @@ namespace AgriBundle\Controller;
 use AgriBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Article controller.
